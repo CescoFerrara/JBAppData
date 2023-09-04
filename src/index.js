d64f9ef83047fe98d6e04ccfd38951f5cbc887fc
Francesco Ferrara <fferrar4@jhu.edu>
2023-09-04 14:39:49 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import jobs from "./routes/JBApps.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
